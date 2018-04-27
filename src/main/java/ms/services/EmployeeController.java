6b9ef65d08459779e158aa3f4c3077b807629189
musawar <musawar86@gmail.com>
2018-04-27 12:07:26 +0200

    Employee Controller, Repository and Entity

---
+++ src/main/java/ms/services/EmployeeController.java
@@ -0,0 +1,55 @@
+package ms.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import ms.entities.Employee;
+import ms.repository.EmployeeRepository;
+
+@RequestMapping("/emp")
+@Api(value = "emp", description = "Employee Information")
+@RestController
+public class EmployeeController {
+
+	@Autowired
+	EmployeeRepository empRepository;
+
+	@ApiOperation(value = "list all Employees", response = Iterable.class)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully retrieved list"),
+			@ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+			@ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+			@ApiResponse(code = 404, message = "The resource you were trying to reach is not found") })
+	
+	@RequestMapping(value = "/getAllEmployee", method = RequestMethod.GET)
+	public List<Employee> getAllDepartments() {
+		return empRepository.getAllEmployees();
+	}
+
+	@ApiOperation(value = "Get an Employee with EMPNO", response = Employee.class)
+	@RequestMapping(value = "/getEmpById/{empno}", method = RequestMethod.GET)
+	public Employee getDeptById(@PathVariable Integer empno) {
+		return empRepository.getByEmptNo(empno);
+	}
+	
+ 
+    
+    @ApiOperation(value = "Delete an Employee")
+    @RequestMapping(value = "/deleteEmp/{empno}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> dellDept(@PathVariable Integer empno){
+    	empRepository.delEmpById(empno);
+        return new ResponseEntity<Object>("Employee deleted successfully", HttpStatus.OK);
+    }
+    
+ 
+	
+}
