9e3613bdce4bb35f27478261319ff6f9b68218e2
musawar <musawar86@gmail.com>
2018-04-27 10:15:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring-boot-h2 Swagger
+In this example I have used H2 as in memory database and rest web services are exposed via Swagger-UI.
---
+++ src/main/java/ms/services/DeptController.java
@@ -0,0 +1,66 @@
+package ms.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import ms.entities.Dept;
+import ms.repository.DeptRepository;
+
+@RequestMapping("/dept")
+@Api(value = "dept", description = "Department Information")
+@RestController
+public class DeptController {
+
+	@Autowired
+	DeptRepository deptRepository;
+
+	@ApiOperation(value = "list all Departments", response = Iterable.class)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully retrieved list"),
+			@ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+			@ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+			@ApiResponse(code = 404, message = "The resource you were trying to reach is not found") })
+	
+	@RequestMapping(value = "/getAllDept", method = RequestMethod.GET)
+	public List<Dept> getAllDepartments() {
+		return deptRepository.getAllDept();
+	}
+
+	@ApiOperation(value = "Get a Department with DEPTNO", response = Dept.class)
+	@RequestMapping(value = "/getDeptById/{deptno}", method = RequestMethod.GET)
+	public Dept getDeptById(@PathVariable Integer deptno) {
+		return deptRepository.getByDeptNo(deptno);
+	}
+	
+    @ApiOperation(value = "Add a Department")
+    @RequestMapping(value = "/insertDept", method = RequestMethod.POST)
+    public ResponseEntity<Object> addDept(@RequestBody Dept dept){
+        deptRepository.addDept(dept);
+        return new ResponseEntity<Object>("Department saved successfully", HttpStatus.OK);
+    }
+    
+    @ApiOperation(value = "Delete a Department")
+    @RequestMapping(value = "/deleteDept/{deptno}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> dellDept(@PathVariable Integer deptno){
+        deptRepository.delDeptById(deptno);
+        return new ResponseEntity<Object>("Department deleted successfully", HttpStatus.OK);
+    }
+    
+    @ApiOperation(value = "Update a Department")
+    @RequestMapping(value = "/updateDept", method = RequestMethod.PUT)
+    public ResponseEntity<Object> updateDept(@RequestBody Dept dept){
+    	deptRepository.update(dept);
+        return new ResponseEntity<Object>("Department updated successfully", HttpStatus.OK);
+    }
+	
+}
